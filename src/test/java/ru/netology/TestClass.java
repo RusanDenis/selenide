228b02aac62192908b837800a0c6af021dd273cb
RusanDenis <rusandenis18@gmail.com>
2025-11-05 17:04:08 +0300

    date added to verification

--- src/test/java/ru/netology/TestClass.java
+++ src/test/java/ru/netology/TestClass.java
@@ -11,6 +11,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -23,13 +24,15 @@ public class TestClass {
     void shouldRegistrationReserv() {
 
         Selenide.open("http://localhost:9999");
-        SelenideElement form = $("form");
+
         $(" [data-test-id='city'] input").setValue("Москва");
         $(" [data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE).setValue(genDate(4, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79999999999");
         $(" [data-test-id='agreement']").click();
-        $$("button").findBy(Condition.exactText("Забронировать")).click();
-        $("[data-test-id='notification']").should(Condition.text("Успешно"), Duration.ofSeconds(15)).should(Condition.visible);
+        $$("button").find(text("Забронировать")).click();
+        $("[data-test-id='notification']").should(text("Успешно"), Duration.ofSeconds(15)).should(Condition.visible)
+                .shouldHave(text(genDate(4, "dd.MM.yyyy")));
+
     }
 }
